9968c14ccf97b3b920303e8ea2a21172ac016da5
peter <pfranciskoe@gmail.com>
2020-04-28 13:08:15 -0700

    created session actions

---
+++ DTCoffee/frontend/actions/session.js
@@ -0,0 +1,27 @@
+import { logIn, logOut, signUp } from '../util/session_api_util';
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+
+const receiveCurrentUser = user =>({
+    type: RECEIVE_CURRENT_USER,
+    user
+})
+
+const logoutCurrentUser = ()=> ({
+    type: LOGOUT_CURRENT_USER,
+})
+
+const receiveErrors = errors => ({
+    type: RECEIVE_SESSION_ERRORS,
+    errors
+})
+
+export const login = formUser => dispatch => logIn(formUser)
+    .then(user => dispatch(receiveCurrentUser(user)), errors => dispatch(receiveErrors(errors.responseJSON)));
+
+export const createNewUser = formUser => dispatch => signUp(formUser)
+    .then(user => dispatch(receiveCurrentUser(user)), errors => dispatch(receiveErrors(errors.responseJSON)));
+
+export const login = formUser => dispatch => logOut(formUser)
+    .then( ()=> dispatch(logoutCurrentUser()), errors => dispatch(receiveErrors(errors.responseJSON)));
