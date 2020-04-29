930e67da3335064e451444ec5a61b308d68e4ad0
peter <pfranciskoe@gmail.com>
2020-04-28 17:41:24 -0700

    changed Root name

--- frontend/dtcoffee.jsx
+++ frontend/dtcoffee.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Root from './components/root';
+import Root from './components/Root';
 import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
